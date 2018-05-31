fa5b14151c7257f066084ed9f43b5bc818fbd5fa
YongJun Kim <StaySharp0@tripbtoz.com>
2018-06-01 02:18:43 +0900

    Update-gateway: gateway 시작시 주기 설정 및 주기 함수 변경

--- gateway/Sensor.py
+++ gateway/Sensor.py
@@ -11,7 +11,6 @@ class Sensor(threading.Thread):
 		self.lock = threading.Lock()
 		self.baseHost='localhost'
 		self.basePort = 9000
-		
 		self.baseList = {} # key: baseId / value : { }
 		self.serverList = {} # key: sid / value : baseList item
 		self.bs_soc = socket(AF_INET, SOCK_STREAM)
@@ -21,19 +20,6 @@ class Sensor(threading.Thread):
 			# setting db
 			self.con = pymysql.connect(host=self.baseHost, port=3306, user='root', passwd='1q2w3e', db='SENSOR_LIST', charset='utf8')
 			self.cur = self.con.cursor()
-			self.cur.execute("select * from sensor where sid!=0")
-			for r in self.cur.fetchall():
-				self.baseList[r[0]] = {
-					'mid': r[0].split(":")[0],
-					'bid': r[0],
-					'type': r[0].split(":")[1],
-					'value': 0,
-					'name': r[3],
-					'sid': r[4],
-				}
-
-				if r[4] != 0:
-					self.serverList[r[4]] = self.baseList[r[0]]
 
 			# setting baseTCP
 			self.bs_soc.connect((self.baseHost, self.basePort))
@@ -44,6 +30,24 @@ class Sensor(threading.Thread):
 			self.bs_soc.send(msg.encode())
 			print("Connected")
 			self._getSensorList()
+
+			self.cur.execute("select * from sensor where sid!=0")
+			for r in self.cur.fetchall():
+				self.baseList[r[0]] = {
+					'mid': r[0].split(":")[0],
+					'bid': r[0],
+					'type': r[0].split(":")[1],
+					'name': r[3],
+					'interval': r[4],
+					'sid': r[5],
+				}
+
+				if r[5] != 0:
+					self.serverList[r[5]] = self.baseList[r[0]]
+				if r[4]:
+					self.updateSensorInterval(r[5],r[4])
+			print(self.baseList)
+
 		except Exception as e:
 			print(e)
 			return
@@ -60,17 +64,17 @@ class Sensor(threading.Thread):
 			data = self.bs_soc.recv(length)
 			data = data[8:].decode().replace('\0', '')
 			buffer += data
-			print(buffer)
+
 			pos = buffer.find('|')
 			if pos != -1:
 				spos = buffer.find('{')
 				msg = buffer[spos:pos].replace('\0', '')
+				print(msg)
 				msg = json.loads(msg)
 				buffer = buffer[pos+1:]
 			else:
 				continue
 			
-			print(msg)
 			if msg["status"] == "list":
 				self._add(msg["id"], msg["type"])
 			elif msg["status"] == "value":
@@ -79,7 +83,7 @@ class Sensor(threading.Thread):
 	def _getSensorList(self):
 		# self.msg[8] = 0x01
 		# self.bs_soc.send(''.join([chr(d) for d in self.msg]).encode())
-		os.system('~/mysend localhost 9000 01')
+		os.system('/home/pi/mysend localhost 9000 01')
 	
 	def _add(self, moteID, sType):
 		with self.lock:
@@ -99,8 +103,8 @@ class Sensor(threading.Thread):
 				}
 				ipt = tuple(self.baseList[baseID].values())
 
-				insert_command = "insert into sensor (id, type, gid, name, sid) values (%s, %s, %s, %s, %s) on duplicate key update id=values(id)"
-				self.cur.execute(insert_command, (baseID, sType, "0", baseID, "0"))
+				insert_command = "insert into sensor (id, type, gid, name, sid, interval) values (%s, %s, %s, %s, %s, %s) on duplicate key update id=values(id)"
+				self.cur.execute(insert_command, (baseID, sType, "0", baseID, "0", "30"))
 				self.con.commit()
 				self.Socket._emit_createSensor(self.baseList[baseID])
 	
@@ -127,8 +131,15 @@ class Sensor(threading.Thread):
 
 	def refreshSensor(self, sid):
 		with self.lock:
+			print('refresh')
 			moteID = self.serverList[sid]['bid'].split(':')[0]
-			os.system('~/mysend localhost 9000 02 %s' % (moteID))
+			sType = self.serverList[sid]['type']
+ 
+			if sType == 'Temperature':
+				sType = '01'
+			elif sType == 'Humidity': 
+			 	sType = '02'
+			os.system('/home/pi/mysend localhost 9000 02 %s %s' % (moteID, sType))
 
 	def updateSensorName(self, sid, name):
 		with self.lock:
@@ -142,6 +153,24 @@ class Sensor(threading.Thread):
 		with self.lock:
 			print('updateSensorInterval')
 			moteID = self.serverList[sid]['bid'].split(':')[0]
-			os.system('~/mysend localhost 9000 04 %s %s' % (moteID, interval))
+			sType = self.serverList[sid]['type']
+			interval = int(interval)
+
+			update_sid_command = "update sensor set sensor.interval=%s where sid=%s"
+			self.cur.execute(update_sid_command, (interval, sid))
+			self.con.commit()
+ 
+			if sType == 'Temperature':
+				sType = '04'
+			elif sType == 'Humidity': 
+			 	sType = '08'
+			
+			os.system('/home/pi/mysend localhost 9000 %s %s %x' % (sType, moteID, interval))
+	
+	def sendIRSerial(self, sid, ir_raw_data):
+		with self.lock:
+			print('sendIRSerial')
+			moteID = self.serverList[sid]['bid'].split(':')[0]
+			os.system('/home/pi/mysend localhost 9000 04 %s %s' % (moteID, str(ir_raw_data)))   ## Fix!!!!
 
 		
